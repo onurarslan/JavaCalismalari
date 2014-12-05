c27f54fd861d22255725a3cea9c13f60b7613694
admin <admin@Lab-1-Server>
2014-12-05 14:56:52 +0200

    ilkDeneme

---
+++ JavaCalismaları/src/Hafta01/Ornek01.java
@@ -0,0 +1,15 @@
+package Hafta01;
+
+public class Ornek01 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		System.out.println("BTBS");
+		System.out.println("Java");
+		
+		System.out.println("Sayý: "+5);
+
+	}
+
+}
